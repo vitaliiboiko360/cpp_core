6ed80b751a516874a460a98fd32bf0a857ac8175
vitaliiboiko360 <vukelan@gmail.com>
2019-02-03 18:33:56 +0200

    snake curses

--- c/x00/main.c
+++ c/x00/main.c
@@ -1,42 +1,88 @@
+#include <string.h>
+#include <
 #include <curses.h>
 
-void print_border();
-void count_down();
+static const int SNAKE_SIZE = 6;
+static const int FOUR = 4; // four directions to move snake's head
+static const char CH = 'x';
+
+void print_snake();
 
 int main(int argc, char* argv)
 {
     initscr();
 
-    while(true)
-    {
-        count_down(0);
-        print_border();
-        
-    }
-
+    print_snake();
+    
     endwin();
     return 0;
 }
 
-void print_border()
+typdef struct
 {
-    clear();
-    wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
-    refresh();
-    return;
+    int x;
+    int y;
+} point;
+
+typedef struct 
+{
+    point snake_points[SNAKE_SIZE];
+} snake;
+
+void move_snake(snake * p_snake);
+point next_move_head(snake * p_snake);
+bool is_valid_move(int x, int y);
+
+void print_snake()
+{
+    snake snake_1;
+    while(true)
+    {
+        move_snake(&snake_1);
+    }
 }
 
-void count_down(int sec)
+void move_snake(snake * p_snake)
 {
-    sec = 1000;
-    int i = 0;
+    mvdelch(p_snake->snake_points[0].y, p_snake->snake_points[0].x);
 
-    while(i < sec)
+    memmove(p_snake->snake_points,
+            p_snake->snake_points+1, 
+            (SNAKE_SIZE-1)*sizeof(point));
+
+    p_snake->snake_points[SNAKE_SIZE-1] = next_move_head(p_snake);
+
+    mvaddch(p_snake->snake_points[SNAKE_SIZE-1].y,
+            p_snake->snake_points[SNAKE_SIZE-1].x, CH);
+    
+    napms(100);
+}
+
+point next_move_head(snake * p_snake)
+{
+    point head = p_snake->snake_points[SNAKE_SIZE-1];
+
+    for(int i=0; i<n_directions; ++i)
     {
-        mvprintw(1, 1, "%s", "     ");
-        mvprintw(1, 1, "%d", sec-i);
-        refresh();
-        napms(10);
-        i += 10;
     }
+}
+
+bool is_valid_move(int x, int y, snake * p_snake)
+{
+    int cols, lines;
+    getmaxyx(stdscr, &lines, &cols);
+
+    if((x >= lines || x < 0)) || (y >= cols || y < 0))
+        return false;
+
+    point * pts = p_snake->snake_points;
+    for(int i=0; i<SNAKE_SIZE; ++i)
+    {
+        if(pts[i].x == x && pts[i].y == y)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
