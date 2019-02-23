32b9a5c74636d24cc379addbf00bf834e9e613b6
vitaliiboiko360 <vukelan@gmail.com>
2019-02-23 23:10:10 +0200

    static class members

--- cpp/x00/class_1.hpp
+++ cpp/x00/class_1.hpp
@@ -9,29 +9,62 @@ public:
     Class_1(Class_1& ref);
     Class_1(Class_1&& rref);
     ~Class_1();
+
+private:
+    void OutId();
+    void OutId2(Class_1& ref);
+    void SetNewId();
+    static int m_counter;
+    int m_id;
 };
 
 Class_1::Class_1()
 {
-    std::cout<<"Class_1() "<<std::hex<<this<<"\n";
+    std::cout<<"Class_1()";
+    SetNewId();
+    OutId();
 }   
 
 Class_1::Class_1(const Class_1& ref)
 {
-    std::cout<<"Class_1(const Class_1& ref)\n";
+    std::cout<<"Class_1(const Class_1& ref)";
+    SetNewId();
+    OutId();
 }
 
 Class_1::Class_1(Class_1& ref)
 {
-    std::cout<<"Class_1(Class_1& ref)\n";
+    std::cout<<"Class_1(Class_1& ref)";
+    SetNewId();
+    OutId();
 }
 
 Class_1::Class_1(Class_1&& rref)
 {
-    std::cout<<"Class_1(Class_1&& rref)\n";
+    std::cout<<"Class_1(Class_1&& rref)";
+    SetNewId();
+    OutId2(rref);
 }
 
 Class_1::~Class_1()
 {
-    std::cout<<"~Class_1()\n";
+    std::cout<<"~Class_1()";
+    OutId();
+}
+
+void Class_1::OutId()
+{
+    std::cout<<" "<<m_id<<"\n";
+}
+
+int Class_1::m_counter = 1;
+
+void Class_1::SetNewId()
+{
+    m_id = Class_1::m_counter++;
+}
+
+void Class_1::OutId2(Class_1& ref)
+{
+    std::cout<<" "<<m_id<<" from "<<ref.m_id<<"\n";
 }
--- cpp/x00/main.cpp
+++ cpp/x00/main.cpp
@@ -8,11 +8,15 @@ int main(int argc, char** argv)
 {
 
     std::vector<Class_1> v1;
-    v1.reserve(3);
+
     std::cout<<"we had reserve\n";
 
-    for(int i=0; i<3; i++)
+    for(int i=0; i<8; i++)
+    {
+        std::cout<<"b: "<<v1.capacity()<<"\n";
         v1.push_back(Class_1());
+        std::cout<<"a: "<<v1.capacity()<<"\n";
+    } 
 
     return 0;
 }
