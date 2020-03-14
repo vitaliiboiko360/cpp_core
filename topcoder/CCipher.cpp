31d82108cca9f2d0919beb90edffcb5563c35989
vitaliiiboiko360 <vitaliiboiko360@gmail.com>
2020-03-14 17:38:14 +0200

    CCipher

---
+++ topcoder/CCipher.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+
+class CCipher
+{
+public:
+	string decode(string cipherText, int shift)
+	{
+		string ret;
+		int norm{ 0 };
+		for(char c : cipherText)
+		{
+			// any (c-shift) must give only 65...90
+			if ( (c - shift) <= 64 )
+			{
+				norm = 90 - (64-(c - shift));
+				ret.push_back(char(norm));
+			}
+			else
+				ret.push_back(c-shift);
+		}
+		
+		/*
+		char c = 'A';
+		int i = 0;
+		while(c != '[')
+		{
+			std::cout<<i<<" "<<c<<std::endl;
+			c++; i++;
+		}*/
+		
+		return ret;
+	}
+};
