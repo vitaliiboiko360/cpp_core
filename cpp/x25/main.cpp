5bc9360d4f94353e2da6912c441e583c6a07092f
vitalii boiko <vukelan@gmail.com>
2020-08-22 20:59:13 +0000

    threads

---
+++ cpp/x25/main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <thread>
+
+int main(int argc, char* argv[])
+{
+
+   std::cout<<std::this_thread::get_id()<<"\n";
+                                                                                                                                                                                                                                                                           
+
+    return 0;
+}
