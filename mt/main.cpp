e9d3482c6164d7b2b7315dd8af3bc7e572f76cc2
vitalii boiko <vukelan@gmail.com>
2020-08-28 19:52:08 +0000

    wrap in thread

--- mt/main.cpp
+++ mt/main.cpp
@@ -1,22 +1,30 @@
 #include <fstream>
 #include <iostream>
 #include <cstdlib>
+#include <thread>
 
 int main(int argc, char* argv[])
 {
+    auto create_file = []{
+        std::ofstream file("file");
 
-    std::ofstream file("file");
-
-    for (int i=0; i<100; ++i)
-    {
-        file << std::rand() << " ";
-        
-        
-        if ((i%10==0) && i!=0)
+        for (int i=0; i<100; ++i)
         {
-            file << '\n';
-            std::cout << file.tellp() << "\n";
+            file << std::rand() << " ";
+            
+            
+            if ((i%10==0) && i!=0)
+            {
+                file << '\n';
+                std::cout << file.tellp() << "\n";
+            }
         }
-    }
-    std::cout << file.tellp() << "\n";
+        std::cout << file.tellp() << "\n";
+    };
+    
+    std::cout<<"before thread\n";
+    std::thread l_thread{create_file};
+
+    std::cout<<"after thread\n";
+    l_thread.join();
 }
