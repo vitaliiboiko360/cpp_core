34b814a5ff5e4f915db6a0fd2fb20316b909da8f
vitalii boiko <vukelan@gmail.com>
2020-09-07 19:22:17 +0000

    rework loops

--- c/a017/main.c
+++ c/a017/main.c
@@ -19,24 +19,26 @@ void* generate_random_stream(void* arg)
     int current_size = 0;
     char buffer[BUFFER_SIZE];
     int bytes_read = 0;
-    int counter = 0;
-    const int iteration_limit = 1000;
-    while(1)
-    {
-        bytes_read = snprintf(buffer+current_size, max(0, BUFFER_SIZE-current_size), "%d ", rand());
-        if (bytes_read == -1)
-        {
-            perror("snprintf");
-            break;
-        }
-        current_size += bytes_read;
-        //sleep(1);
+    
+    printf("before loop in thread\n");
 
-        if(current_size > BUFFER_SIZE*3/4)
+    int i = 0;
+    while(i++ < 3)
+    {
+        while (current_size < BUFFER_SIZE*3/4)
         {
-            printf("%s \n", buffer);
-            current_size = 0;
+            bytes_read = snprintf(buffer+current_size, max(0, BUFFER_SIZE-current_size), "%d ", rand());
+            if (bytes_read == -1)
+            {
+                perror("snprintf");
+                break;
+            }
+            current_size += bytes_read;
         }
+
+        printf("%s \n", buffer);
+        current_size = 0;
+        sleep(1);
     }
     pthread_detach(pthread_self());
     printf("THREAD REACHED ITS END\n");
@@ -54,7 +56,7 @@ int main(int argc, char* argv[])
     str_1[size_1] = '\0';
 
     int i=0;
-    while(++i < 10)
+    while(++i < 6)
     {
         sleep(1);
         printf("MSG from main %d \n%s\n", i, str_1);
