61dc6ec145dbc4f2374be7d8a6e8fb82c9f8d9e1
vitalii boiko <vukelan@gmail.com>
2020-09-14 18:32:19 +0000

    thread join

---
+++ cpp/x27/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <thread>
+
+void busy_work()
+{
+    int acc;
+    for(int i=0; i<__INT16_MAX__; i++)
+    {   
+        if (acc % 2 ==0)
+            acc += i;
+    }
+}
+
+void thread_function(int arg)
+{
+    std::cout<<"this thread would count to number \"arg\"= "<<arg<<"\n";
+    for(int i=0; i<arg; i++)
+    {
+        std::cout<<"i= "<<i<<"\n";
+        busy_work();
+    }
+}
+
+int main(int argc, char* argv[])
+{
+
+    int number = 500;
+
+    std::thread thread_1(thread_function, number);   
+    for(int i=0; i<number; i++)
+    {
+        std::cout<<"main i="<<i<<"\n";
+        busy_work();
+    }
+    thread_1.join();
+
+    return 0;
+}
