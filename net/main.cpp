6fc6246263c42dc380daafd02f222165aeb2a5ab
vitaliiboiko360 <vukelan@gmail.com>
2020-09-21 19:47:41 +0000

    test echo

--- net/main.cpp
+++ net/main.cpp
@@ -27,6 +27,11 @@ int main(int argc, char* argv[])
     if (argc > 1 && 
         *argv[1] == '-')
     {
+        if (argv[1][1] == 'c' && argv[1][2] == 'e')
+        {
+            u_client client;
+            return client.run_echo_cli();
+        }
         if (argv[1][1] == 'c')
         {
             u_client client;
