8054ec987710b2c81b4e6a2b89a8660765454fb1
vitaliiboiko360 <vukelan@gmail.com>
2020-10-01 20:08:57 +0000

    bitwize shifts

--- cpp/x31/main.cpp
+++ cpp/x31/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdarg>
+#include <climits>
 
 const int ID_SIZE = 3;
 
@@ -45,15 +46,22 @@ A func_2(A a)
     return a;
 }
 
+
+
 int main(int argc, char* argv[])
 {
-    A* p_a1;
-    std::cout<<"sizeof(ptr)= "<<sizeof(p_a1)<<"\n";
-    A a;
-    A a1;
-    a1 = a;
-    func(a);
-    func_2(a);
+
+    std::cout<<INT_MAX<<"\n";
+    int a = INT_MAX;
+    unsigned char c;
+    for(int i=0; i<32; i += 4)
+    {
+        c = 0;
+        c = (INT_MAX<<i) | c;
+        std::cout<<"(INT_MAX<<"<<i<<")="<<(INT_MAX<<i)<<"\n";
+        std::cout<<c<<"\n";
+    }
+
 
     return 0;
 }
