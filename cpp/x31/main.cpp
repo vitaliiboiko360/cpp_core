18852d1cf098aac872517454ce7d10074023ad16
vitaliiboiko360 <vukelan@gmail.com>
2020-10-02 20:35:22 +0000

    copy ctor invokes

--- cpp/x31/main.cpp
+++ cpp/x31/main.cpp
@@ -12,22 +12,33 @@ public:
     A()
     : id()
     {
-        std::cout<<"A()\n";
+        std::cout<<std::hex<<this<<" A()\n";
     }
 
     ~A()
     {
-        std::cout<<"~A()\n";
+        std::cout<<std::hex<<this<<" ~A()\n";
     }
 
     A(const A& a)
     {
-        std::cout<<"A(const A& a)\n";
+        std::cout<<std::hex<<this<<" A(const A& a)\n";
     }
 
     A& operator=(const A& a)
     {
-        std::cout<<"A operator=(const A& a)\n";
+        std::cout<<std::hex<<this<<" A operator=(const A& a)\n";
+        return *this;
+    }
+
+    A(A&& a)
+    {
+        std::cout<<std::hex<<this<<" A(A&& a)\n";
+    }
+
+    A& operator=(A&& a)
+    {
+        std::cout<<std::hex<<this<<" A& operator=(A&& a)\n";
         return *this;
     }
 
@@ -51,17 +62,14 @@ A func_2(A a)
 int main(int argc, char* argv[])
 {
 
-    std::cout<<INT_MAX<<"\n";
-    int a = INT_MAX;
-    unsigned char c;
-    for(int i=0; i<32; i += 4)
-    {
-        c = 0;
-        c = (INT_MAX<<i) | c;
-        std::cout<<"(INT_MAX<<"<<i<<")="<<(INT_MAX<<i)<<"\n";
-        std::cout<<c<<"\n";
-    }
+    A a;
+    A a1;
 
+    func(a);
+    func_2(a1);
+
+    a = a1;
+    A a2 = a;
 
     return 0;
 }
