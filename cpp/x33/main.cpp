dcf1a52abdf29bc4f8a7dfd1df80d9257fb99ba9
vitaliiboiko360 <vukelan@gmail.com>
2020-10-07 12:57:21 +0000

    copy and move

--- cpp/x33/main.cpp
+++ cpp/x33/main.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+#include <vector>
 
-static auto v = 7;
+class A
+{
+public:
+    A()
+    : myid(++id)
+    {
+        print_id("A()");
+    }
+    ~A()
+    {
+        print_id("~A()");
+    }
+    A(const A& a)
+    : myid(++id)
+    {
+        print_id("A(const A& a)", a.myid);
+    }
+    A(A&& a)
+    : myid(++id)
+    {
+        print_id("A(A&& a)", a.myid);
+    }
+
+    A& operator= (A&& rref)
+    {
+        print_id("A& operator= (A&& rref)", rref.myid);
+        return *this;
+    }
+
+    int myid;
+    
+private:
+    static int id;
+
+    void print_id(const std::string& str = "", int others_id = -1)
+    {
+        std::cout<<myid<<" "<<str;
+        if (others_id != -1)
+            std::cout<<" "<<others_id;
+        std::cout<<"\n";
+    }
+};
+
+int A::id = 0;
 
 int main()
 {
 
-
-    int array[] = {1,2,3};
-
-    auto i = array[2];
-    auto* p = array;
-
-    std::cout<<i<<"\n";
-    std::cout<<*p<<"\n";
-    std::cout<<v<<"\n";
+    std::vector<A> s;
+    for(int i=0; i<5; ++i)
+        s.push_back(A());
 
 }
