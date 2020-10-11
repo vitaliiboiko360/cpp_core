d40bbb0e80456e2d8cb47fa4d890e4b198871bdd
vitaliiboiko360 <vukelan@gmail.com>
2020-10-11 19:39:07 +0000

    net socket srv

--- cpp/x35/main.cpp
+++ cpp/x35/main.cpp
@@ -1,3 +1,4 @@
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -6,30 +7,41 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <iostream>
+
+
+void chck_error_code(int ec, const char* msg)
+{
+    if (ec == -1)
+    {
+        std::cout<<msg<<"\n";
+        exit(1);
+    }
+}
+
 int main()
 {
-
+    const int max_conns_num = 10;
     const int server_port = 19000;
     const int settings_number = 10;
     int settings[settings_number];
 
-    int sfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sfd == -1)
-    {
-        printf("socket");
-        exit(1);
-    }
+    int sfd;
+    chck_error_code(
+        sfd = socket(AF_INET, SOCK_STREAM, 0),
+        "socket");
 
     struct sockaddr_in srv_addr;
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_port = htons(server_port);
-    srv_addr.sin_addr.s_addr = INADDR_ANY;
-    memset(&srv_addr, 0, sizeof(struct sockaddr_in));
-    int ec = bind(sfd, (struct sockaddr*)&srv_addr, sizeof(srv_addr));
-    if (ec == -1)
-    {
-        printf("socket");
-        exit(1);
-    }
+
+    inet_pton(AF_INET, "0.0.0.0", &srv_addr.sin_addr.s_addr);
+
+    chck_error_code(
+        bind(sfd, (struct sockaddr*)&srv_addr, sizeof(srv_addr)),
+        "bind");
+
+    chck_error_code(listen(sfd, max_conns_num), "listen");
+
     close(sfd);
 }
