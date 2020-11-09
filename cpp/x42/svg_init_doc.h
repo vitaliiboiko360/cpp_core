0541f8ed78762a3791ba23fec20b1c7f26022122
vitaliiboiko360 <vukelan@gmail.com>
2020-11-09 21:37:52 +0000

    svg

--- cpp/x42/svg_init_doc.h
+++ cpp/x42/svg_init_doc.h
@@ -1,15 +1,6 @@
 #include <string>
 
 const std::string svg_init_doc = {R"_(<svg height="600" width="800">
-                <g>
-                    <defs>
-                        <radialGradient id="grad1" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
-                        <stop offset="0%" style="stop-color:rgb(255,255,255);
-                        stop-opacity:0" />
-                        <stop offset="100%" style="stop-color:rgb(0,0,255);stop-opacity:1" />
-                        </radialGradient>
-                    </defs>
-                    <ellipse cx="{CX}" cy="{CY}" rx="85" ry="55" fill="url(#grad1)" />
-                    <text x="{CX}" y="{CY}" fill="black">{TEXT}</text>
-                </g>
+                <path d="M 100 350 q 150 -300 300 0" stroke="black"
+  stroke-width="5" fill="none" />
                 </svg>)_"};
