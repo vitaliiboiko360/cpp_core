557dff27fb32c159a6cf45081f2a214a67cb5c7b
vitaliiboiko360 <vukelan@gmail.com>
2020-11-10 21:40:11 +0000

    svg

--- cpp/x42/svg_init_doc.h
+++ cpp/x42/svg_init_doc.h
@@ -1,6 +1,6 @@
 #include <string>
 
 const std::string svg_init_doc = {R"_(<svg height="600" width="800">
-                <path d="M 100 350 q 150 -300 300 0" stroke="black"
-  stroke-width="5" fill="none" />
+                <path d="M 100 350 q 150 -300 300 0" stroke="black" stroke-width="5" fill="none" />
+                <circle cx="100" cy="100" r="50" stroke="black" stroke-width="5" fill="red" />
                 </svg>)_"};
