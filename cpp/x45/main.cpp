5d58c9c6b053750568c0cf90ee615434419b5d57
vitaliiboiko360 <vukelan@gmail.com>
2020-11-18 21:57:22 +0000

    cleanup

--- cpp/x45/main.cpp
+++ cpp/x45/main.cpp
@@ -4,47 +4,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int
-main(
-int argc, 
-char **argv
-)
+int main(int argc, char **argv)
 {
-    unsigned short port;       /* port server binds to                */
-    char buf[12];              /* buffer for sending & receiving data */
-    struct sockaddr_in client; /* client address information          */
-    struct sockaddr_in server; /* server address information          */
-    int s;                     /* socket for accepting connections    */
-    int ns;                    /* socket connected to client          */
-    int namelen;               /* length of client name               */
-
-    /*
-     * Check arguments. Should be only one: the port number to bind to.
-     */
-
+    int ns,s;
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s port\n", argv[0]);
         exit(1);
     }
 
-    /*
-     * First argument should be the port.
-     */
-    port = (unsigned short) atoi(argv[1]);
-
-    /*
-     * Get a socket for accepting connections.
-     */
+    unsigned short port = (unsigned short) atoi(argv[1]);
     if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("Socket()");
         exit(2);
     }
 
-    /*
-     * Bind the socket to the server address.
-     */
+    struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port   = htons(port);
     server.sin_addr.s_addr = INADDR_ANY;
@@ -55,37 +31,27 @@ char **argv
         exit(3);
     }
 
-    /*
-     * Listen for connections. Specify the backlog as 1.
-     */
     if (listen(s, 1) != 0)
     {
         perror("Listen()");
         exit(4);
     }
 
-    /*
-     * Accept a connection.
-     */
-    namelen = sizeof(client);
+    struct sockaddr_in client;
+    int namelen = sizeof(client);
     if ((ns = accept(s, (struct sockaddr *)&client, (socklen_t *)&namelen)) == -1)
     {
         perror("Accept()");
         exit(5);
     }
 
-    /*
-     * Receive the message on the newly connected socket.
-     */
+    char buf[12];
     if (recv(ns, buf, sizeof(buf), 0) == -1)
     {
         perror("Recv()");
         exit(6);
     }
 
-    /*
-     * Send the message back to the client.
-     */
     if (send(ns, buf, sizeof(buf), 0) < 0)
     {
         perror("Send()");
