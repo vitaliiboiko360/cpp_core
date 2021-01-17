a0ee78d6cbc21405e0b6b0eef06faf080c0af921
vitaliiboiko360 <vukelan@gmail.com>
2021-01-17 19:02:06 +0000

    add thread

--- cpp/x50/main.cpp
+++ cpp/x50/main.cpp
@@ -1,12 +1,22 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 #include "http_srv.h"
 
 int main(int argc, char* argv[])
 {
-    http_server hs;
-    hs.serve();
+    std::thread t([]{
+        http_server hs;
+        hs.serve();
+    });
+  
+    for(int i=0; i<10; i++)
+    {
+        std::cout<<"pretend work "<<i<<"\n";
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 
-    std::cout<<"end\n";
+    t.join();
 
     return 0;
 }
