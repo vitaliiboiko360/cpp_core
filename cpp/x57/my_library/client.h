ca774f84f3be85be0536c669253edbafeba0aef2
vitalii boiko <vukelan@gmail.com>
2021-02-11 20:11:23 +0000

    init cli port

--- cpp/x57/my_library/client.h
+++ cpp/x57/my_library/client.h
@@ -2,6 +2,7 @@
 
 class my_client
 {
+    uint16_t m_port{0};
 public:
     my_client(char* host, int16_t port);
     void run();
--- cpp/x57/my_library/src/client.cpp
+++ cpp/x57/my_library/src/client.cpp
@@ -1,8 +1,10 @@
 #include "../client.h"
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 my_client::my_client(char* host, int16_t port)
 {
-    
+    uint16_t m_port = htons(port);
 }
 
 void my_client::run()
