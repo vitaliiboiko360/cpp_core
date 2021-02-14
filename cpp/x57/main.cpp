82e3c29022d8005191806e5970fa442fd56a82d9
vitalii boiko <vukelan@gmail.com>
2021-02-14 19:02:30 +0000

    separate cli and srv main

--- cpp/x57/main.cpp
+++ cpp/x57/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <thread>
+#include <string.h>
 #include "config.h"
 #include "my_library/client.h"
 #include "my_library/server.h"
@@ -10,25 +11,60 @@
 const uint16_t port_number = 8888;
 const char* localhost = "127.0.0.1";
 
-int main()
+int client_main();
+int server_main();
+
+int main(int argc, char* argv[])
+{
+    auto print_usage = [](){        
+        std::cout<<"usage: ./app -s | -c\n";
+        return 1;
+    };
+    if(argc < 2)
+    {
+        return print_usage();
+    }
+
+    if(0 == strcmp(argv[1],"-s"))
+    {
+        return server_main();
+    }
+    else if(0 == strcmp(argv[1],"-c"))
+    {
+        return client_main();
+    }
+    else
+    {
+        return print_usage();
+    }
+    
+
+    return 0;
+}
+
+int client_main()
 {
     my_client cli0(port_number);
     my_client cli1(port_number);
     my_client cli2(port_number);
     my_client cli3(port_number);
-    my_server srv(localhost, port_number);
 
     std::vector<std::thread> threads;
-
-    threads.emplace_back(std::thread([&srv]{srv.run();}));
     threads.emplace_back(std::thread([&cli0]{cli0.run();}));
-    // threads.emplace_back(std::thread([&cli1]{cli1.run();}));
-    // threads.emplace_back(std::thread([&cli2]{cli2.run();}));
-    // threads.emplace_back(std::thread([&cli3]{cli3.run();}));
-
-    //std::this_thread::sleep_for(std::chrono::seconds(10));
+    threads.emplace_back(std::thread([&cli1]{cli1.run();}));
+    threads.emplace_back(std::thread([&cli2]{cli2.run();}));
+    threads.emplace_back(std::thread([&cli3]{cli3.run();}));
 
     for_each(threads.begin(), threads.end(), [](std::thread& t){t.join();});
+
+    return 0;
+}
+
+int server_main()
+{
+    my_server srv(localhost, port_number);
+    std::vector<std::thread> threads;
+    threads.emplace_back(std::thread([&srv]{srv.run();}));
 
     return 0;
 }
