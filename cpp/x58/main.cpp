4b26bd39f65685522365d9de4b5e7f666af485ea
vitalii boiko <vukelan@gmail.com>
2021-02-18 12:18:12 +0000

    copy ctor

---
+++ cpp/x58/main.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using std::string;
+
+class my_string
+{
+    std::string _str;
+public:
+    my_string()
+    {
+        print_id("my_string()");
+    }
+    my_string(string str)
+    : _str(str)
+    {
+        print_id("my_string(str)");
+    }
+    ~my_string()
+    {
+        print_id("~my_string()");
+    }
+    my_string(const my_string& r)
+    {
+        print_id((void*)&r, "const my_string& =");
+        print_id("my_string(const my_string& r)");
+    }
+
+
+private:
+    void print_id(string msg = "") {std::cout<<msg<<std::hex<<(int64_t(this) & 0xffff)<<std::endl;}
+    void print_id(void* ptr, string msg = "") {std::cout<<msg<<std::hex<<(int64_t(ptr) & 0xffff)<<std::endl;}
+};
+
+my_string indet(my_string arg)
+{
+    return arg;
+}
+
+int main()
+{
+
+    my_string s1{"Adams"};
+    s1 = indet(s1);
+}
