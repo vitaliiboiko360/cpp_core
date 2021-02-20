8fdc1dee2f243498e0b47b29ff1d11c0c2d0a30f
vitalii boiko <vukelan@gmail.com>
2021-02-20 14:13:42 +0000

    adding client ws connetion

--- cpp/x53/html/script.js
+++ cpp/x53/html/script.js
@@ -1,5 +1,29 @@
-
-(function myFunction() {
+(function mainFunction() {
     var titles= document.getElementsByTagName("h1");
-    titles[0].innerHTML = "Heading changed from the script.";
+    titles[0].innerHTML = "Script header";
+
+    var paragraph = document.getElementById("paragraph");
+
+    var buttonConnect = document.getElementById("button_connect");
+    buttonConnect.addEventListener("click", connectWebSocket);
+
+    var webSocketId;
+    function connectWebSocket(){
+      var srvConnectionString = "ws://" + window.location.hostname + ':' + `${parseInt(window.location.port)}`;
+      var webSocket = new WebSocket("ws://localhost:3000");
+      webSocketId = webSocket;
+      webSocket.onmessage = function(event) {
+        paragraph.innerHTML() = event.data
+      }
+    }
+
+    var buttonClose = document.getElementById("button_close");
+    buttonClose.onclick = function() {
+
+      if(webSocketId != undefined)
+      {
+        webSocketId.close();
+      }
+    }
+
   })();
