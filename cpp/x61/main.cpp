1f8b18c598a54382d53efb7149f057dc1b2f5d76
vitalii boiko <vukelan@gmail.com>
2021-03-05 20:11:50 +0000

    init raw socket

--- cpp/x61/main.cpp
+++ cpp/x61/main.cpp
@@ -1,3 +1,17 @@
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <cstdlib>
+
+void if_true_exit(bool check, const char* msg)
+{
+    if(check)
+    {
+        std::cout<<msg<<std::endl;
+        std::exit(1);
+    }
+}
 
 int cli_main();
 int srv_main();
@@ -11,7 +25,9 @@ int main(int argc, char* argv[])
 
 int cli_main()
 {
-
+    int socket_id = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if_true_exit(socket_id == -1, "cli socket");
+    
 }
 
 int srv_main()
