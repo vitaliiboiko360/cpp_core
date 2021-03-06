e97428ae0eb2706020e4aa3b0ab8296dd91729e2
vitalii boiko <vukelan@gmail.com>
2021-03-06 18:36:15 +0000

    prep sockets

--- cpp/x61/main.cpp
+++ cpp/x61/main.cpp
@@ -3,24 +3,61 @@
 #include <sys/socket.h>
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
+#include <stdio.h>
+
+struct udp_header {
+    unsigned short int uh_sport;
+    unsigned short int uh_dport;
+    unsigned short int uh_len;
+    unsigned short int uh_check;
+}; 
 
 void if_true_exit(bool check, const char* msg)
 {
     if(check)
     {
-        std::cout<<msg<<std::endl;
+        perror(msg);
         std::exit(1);
     }
 }
 
-int cli_main();
+void print_usage_exit()
+{
+    std::cout<<"usage: app -c|-s\n";
+    std::exit(1);
+}
+
 int srv_main();
+int cli_main();
 
 int main(int argc, char* argv[])
 {
+    if(argc < 2)
+    {
+        print_usage_exit();
+    }
 
+    if(0 == strcmp(argv[1], "-s"))
+    {
+        return srv_main();
+    }
+    else if(0 == strcmp(argv[1], "-c"))
+    {
+        return cli_main();
+    }
+    else
+    {
+        print_usage_exit();
+    }
+}
 
+int srv_main()
+{
+    int socket_id = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    if_true_exit(socket_id == -1, "cli socket");
 
+    return 0;
 }
 
 int cli_main()
@@ -28,9 +65,5 @@ int cli_main()
     int socket_id = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if_true_exit(socket_id == -1, "cli socket");
     
-}
-
-int srv_main()
-{
-
+    return 0;
 }
